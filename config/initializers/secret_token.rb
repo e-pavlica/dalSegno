0caa6145ef8757565bea3fb602d7d6e671495120
Evan Pavlica <evan@xicreative.net>
2013-10-18 17:26:29 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DalSegno::Application.config.secret_key_base = '8557379425d460529065bcfef8cf6007237227cdbeced3eb572d25e56d6030c5192bf174aa4c209ff5a28a3d187553607ae5b950ff976b78be2ed1472a3ea54d'
