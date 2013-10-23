d96a7ef792854a0a42f615c1f860c4c91852b9f8
Evan Pavlica <evan@xicreative.net>
2013-10-23 09:46:14 -0700

    add some angular, replies now work

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,13 +1,14 @@
 class RepliesController < ApplicationController
+
   def show
     @reply = Reply.find(params[:id])
   end
 
   def create
-    @message = Message.find(params[:message_id]).
-    @reply =  @message.replies.create(params[:reply].permit(:reply_text))
+    @message = Message.find(params[:message_id])
+    @reply = @message.replies.create(params[:reply].permit(:reply_text))
 
-    redirect_to :action => "messages#index"
+    redirect_to messages_path
   end
 
   def new
